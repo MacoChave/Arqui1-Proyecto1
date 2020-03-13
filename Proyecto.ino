3a40713cce047e6ad9755dfa3d6e3c93c0617ac9
Maco Chave <catalantt@gmail.com>
2020-03-12 18:50:26 -0600

    Carita triste

--- Proyecto.ino
+++ Proyecto.ino
@@ -1,5 +1,51 @@
+#include <LedControl.h>
+
+LedControl matrix = LedControl(2,3,4,1);
+
+const int sad_face[][8] =  {
+                        {0, 0, 0, 0, 0, 0, 0, 0}, 
+                        {0, 0, 1, 0, 0, 1, 0, 0}, 
+                        {0, 1, 1, 0, 0, 1, 1, 0}, 
+                        {0, 1, 1, 0, 0, 1, 1, 0}, 
+                        {0, 0, 0, 0, 0, 0, 0, 0}, 
+                        {0, 0, 1, 1, 1, 1, 0, 0}, 
+                        {0, 1, 0, 0, 0, 0, 1, 0}, 
+                        {0, 0, 0, 0, 0, 0, 0, 0},
+                      };
+
 /* NOTAS MUSICALES */
+#define NOTE_B0  31
+#define NOTE_C1  33
+#define NOTE_CS1 35
+#define NOTE_D1  37
+#define NOTE_DS1 39
+#define NOTE_E1  41
+#define NOTE_F1  44
+#define NOTE_FS1 46
+#define NOTE_G1  49
+#define NOTE_GS1 52
+#define NOTE_A1  55
+#define NOTE_AS1 58
+#define NOTE_B1  62
+#define NOTE_C2  65
+#define NOTE_CS2 69
+#define NOTE_D2  73
+#define NOTE_DS2 78
+#define NOTE_E2  82
+#define NOTE_F2  87
+#define NOTE_FS2 93
+#define NOTE_G2  98
+#define NOTE_GS2 104
+#define NOTE_A2  110
+#define NOTE_AS2 117
+#define NOTE_B2  123
+#define NOTE_C3  131
+#define NOTE_CS3 139
+#define NOTE_D3  147
+#define NOTE_DS3 156
 #define NOTE_E3  165
+#define NOTE_F3  175
+#define NOTE_FS3 185
 #define NOTE_G3  196
 #define NOTE_GS3 208
 #define NOTE_A3  220
@@ -9,17 +55,54 @@
 #define NOTE_CS4 277
 #define NOTE_D4  294
 #define NOTE_DS4 311
+#define NOTE_E4  330
 #define NOTE_F4  349
 #define NOTE_FS4 370
+#define NOTE_G4  392
 #define NOTE_GS4 415
 #define NOTE_A4  440
 #define NOTE_AS4 466
+#define NOTE_B4  494
+#define NOTE_C5  523
+#define NOTE_CS5 554
+#define NOTE_D5  587
+#define NOTE_DS5 622
+#define NOTE_E5  659
+#define NOTE_F5  698
+#define NOTE_FS5 740
+#define NOTE_G5  784
+#define NOTE_GS5 831
+#define NOTE_A5  880
+#define NOTE_AS5 932
+#define NOTE_B5  988
+#define NOTE_C6  1047
+#define NOTE_CS6 1109
+#define NOTE_D6  1175
+#define NOTE_DS6 1245
+#define NOTE_E6  1319
+#define NOTE_F6  1397
+#define NOTE_FS6 1480
 #define NOTE_G6  1568
+#define NOTE_GS6 1661
+#define NOTE_A6  1760
+#define NOTE_AS6 1865
+#define NOTE_B6  1976
 #define NOTE_C7  2093
+#define NOTE_CS7 2217
+#define NOTE_D7  2349
+#define NOTE_DS7 2489
 #define NOTE_E7  2637
+#define NOTE_F7  2794
+#define NOTE_FS7 2960
 #define NOTE_G7  3136
-
-#define melodyPin 9
+#define NOTE_GS7 3322
+#define NOTE_A7  3520
+#define NOTE_AS7 3729
+#define NOTE_B7  3951
+#define NOTE_C8  4186
+#define NOTE_CS8 4435
+#define NOTE_D8  4699
+#define NOTE_DS8 4978
 
 int melody[] = {
   NOTE_E7, NOTE_E7, 0, NOTE_E7, 
@@ -27,7 +110,6 @@ int melody[] = {
   NOTE_G7, 0, 0,  0,
   NOTE_G6, 0, 0, 0
 };
-
 int tempo[] = {
   12, 12, 12, 12, 
   12, 12, 12, 12,
@@ -45,7 +127,6 @@ int underworld_melody[] = {
   NOTE_AS3, NOTE_A3, NOTE_GS3,
   0, 0, 0
 };
-
 int underworld_tempo[] = {
   6, 18, 18, 18,
   6, 6,
@@ -62,13 +143,20 @@ int ESTADO = 0; // 0 MONEDAS | 1 TIPO CONTROL | 2 MANUAL | 3 DIGITAL
 int SENSOR_LDR[3] = {A0, A1, A2}; // QUETZAL | VEINTICINCO | CINCUENTA
 int BOTONES[7] = {30, 31, 32, 33, 34, 35}; // +X | -X | +Y | -Y | A ABAJO / ARRIBA | T TOMAR / SOLTAR
 
+int melodyPin 9
+
 char valor = 0;
 bool garra_cerrada = false;
 bool garra_abajo = false;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(9, OUTPUT);
+
+  pinMode(melodyPin, OUTPUT);
+
+  matrix.shutdown(0, false);
+  matrix.setIntensity(0, 3);
+  matrix.clearDisplay(0);
 }
 
 void loop() {
@@ -266,4 +354,19 @@ void buzz(int targetPin, long frequency, long length) {
     digitalWrite(targetPin,LOW); // write the buzzer pin low to pull back the diaphram
     delayMicroseconds(delayValue); // wait againf or the calculated delay value
   }
+}
+
+void printSadFace() {
+  for (int x = 0; x < 1000; x++)
+  {
+    for (int i = 0; i < 8; i++)
+    {
+      for (int j = 0; j < 8; j++)
+      {
+        if (sad_face[i][j] == 1)
+          matrix.setLed(0, i, j, HIGH);
+      }
+    }
+  }
+  matrix.clearDisplay(0);
 }
